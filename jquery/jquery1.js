2310aaa0104a63efe34618843e61df461e40f2c2
Jose Luis Núñez <joseluisnunez@iesamachado.org>
2022-11-15 18:20:52 +0100

    Añadido forma más corta de escribir el ready

--- jquery/jquery1.js
+++ jquery/jquery1.js
@@ -1,7 +1,16 @@
 /* Mi primer ejercicio con JQuery */
+/*
 $(document).ready(function () {
-    /* Lo que sucede aquí dentro ocurre cuando el documento ha terminado de cargar */
+    
    $("button").click(function(){
+        $("h1").css({"background-color":"red", "font-size":"500%", "color":"white", "text-align":"center"});
+   });
+});
+*/
+
+$(function () {
+    /* Forma más corta de escribir el ready */
+    $("button").click(function(){
         $("h1").css({"background-color":"red", "font-size":"500%", "color":"white", "text-align":"center"});
    });
 });
